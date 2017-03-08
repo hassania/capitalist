ef3e4bdd3a4bda69bb5abe7634b0545b34bd4423
AHAMADA HASSANI <ahassani@castres.univ-jfc.fr>
2017-03-08 11:50:21 +0100

    premier commit

---
+++ target/generated-sources/annotations/world/Services_.java
@@ -0,0 +1,13 @@
+package world;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2017-03-08T11:21:14")
+@StaticMetamodel(Services.class)
+public class Services_ { 
+
+    public static volatile SingularAttribute<Services, Long> id;
+
+}
